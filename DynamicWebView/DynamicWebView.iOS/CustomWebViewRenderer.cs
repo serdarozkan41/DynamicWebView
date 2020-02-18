0c898556627498a9164f81b061563d429bea3b61
Serdar Özkan <serdar.ozkan@cmosrealitylab.com>
2020-02-18 15:16:08 +0300

    Scrooll Close

---
+++ DynamicWebView/DynamicWebView.iOS/CustomWebViewRenderer.cs
@@ -0,0 +1,18 @@
+﻿using DynamicWebView.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(WebView), typeof(CustomWebViewRenderer))]
+namespace DynamicWebView.iOS
+{
+    public class CustomWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            var webView = this;
+            webView.ScrollView.ScrollEnabled = false;
+            webView.ScrollView.Bounces = false;
+        }
+    }
+}
